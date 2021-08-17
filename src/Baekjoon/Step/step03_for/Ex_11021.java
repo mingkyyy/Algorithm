d30dfa4828745d4c0586fde0c5816034386f5591
mingkyy <alsrus1227@naver.com>
2021-08-17 16:11:23 +0900

    A+B-7

---
+++ src/Baekjoon/Step/step03_for/Ex_11021.java
@@ -0,0 +1,28 @@
+package Baekjoon.Step.step03_for;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Ex_11021 {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i=1; i<=n; i++){
+            st=new StringTokenizer(br.readLine()," ");
+            int a= Integer.parseInt(st.nextToken());
+            int b= Integer.parseInt(st.nextToken());
+
+            bw.write("Case #"+i+": "+(a+b)+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
