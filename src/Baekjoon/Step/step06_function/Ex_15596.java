90fb4a000449e6e77f1fb157a7af09273db46823
mingkyy <alsrus1227@naver.com>
2021-08-24 16:56:49 +0900

    정수 n개의 합

---
+++ src/Baekjoon/Step/step06_function/Ex_15596.java
@@ -0,0 +1,20 @@
+package Baekjoon.Step.step06_function;
+
+class Test {
+    long sum(int[] a){
+
+        long sum=0;
+
+        for (int i=0; i<a.length; i++){
+            sum+=a[i];
+        }
+        return sum;
+    }
+}
+
+
+public class Ex_15596 {
+    public static void main(String[] args) {
+
+    }
+}
