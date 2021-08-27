8be6d66d813eab619259a732352c76c4592fb752
mingkyy <alsrus1227@naver.com>
2021-08-27 21:30:44 +0900

    아스키 코드

---
+++ src/Baekjoon/Step/step07_string/Ex_11654.java
@@ -0,0 +1,12 @@
+package Baekjoon.Step.step07_string;
+
+import java.util.Scanner;
+
+public class Ex_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.nextLine().charAt(0);
+
+        System.out.println((int)c);
+    }
+}
