93b3cefbb15b48fa0a3d6890484001fd49d34b8b
mingkyy <alsrus1227@naver.com>
2021-11-03 15:54:13 +0900

    다이얼

---
+++ src/Baekjoon/Step/step07_string/Ex_5622.java
@@ -0,0 +1,73 @@
+package Baekjoon.Step.step07_string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ex_5622 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'A':
+                case 'B':
+                case 'C':
+                    sum+=3;
+                    break;
+
+                case 'D':
+                case 'E':
+                case 'F':
+                    sum+=4;
+                    break;
+
+                case 'G':
+                case 'H':
+                case 'I':
+                    sum+=5;
+                    break;
+
+                case 'J':
+                case 'K':
+                case 'L':
+                    sum+=6;
+                    break;
+
+                case 'M':
+                case 'N':
+                case 'O':
+                    sum+=7;
+                    break;
+
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    sum+=8;
+                    break;
+
+                case 'T':
+                case 'U':
+                case 'V':
+                    sum+=9;
+                    break;
+
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    sum+=10;
+                    break;
+
+                default:
+                    sum+=11;
+                    break;
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
