ca1ac893b5181ddfa87c302f788fd66fd875f3d1
mingkyy <alsrus1227@naver.com>
2021-12-11 11:55:15 +0900

    중앙값 구하기

---
+++ src/doit/chap1/Quiz04.java
@@ -0,0 +1,41 @@
+package doit.chap1;
+
+import java.util.Scanner;
+
+public class Quiz04 {
+    static int med3(int a, int b, int c){
+        if (a>=b) {
+            if (c >= a) {
+                return a;
+            }
+            if (b >= c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+        else if (a>c){
+            return a;
+        }else if (b>c){
+            return c;
+        }
+        else
+            return b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("a의 값을 입력하세요");
+        int a = sc.nextInt();
+
+        System.out.println("b의 값을 입력하세요");
+        int b = sc.nextInt();
+
+        System.out.println("c의 값을 입력하세요");
+        int c = sc.nextInt();
+
+        System.out.println("중앙 값은 "+med3(a,b,c)+" 입니다");
+
+    }
+}
